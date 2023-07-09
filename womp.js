4d9130a7bc0e635abf3361a5ffaf651eed9b9a03
Marco <114459798+marcodillanes@users.noreply.github.com>
2023-07-08 21:43:23 -0700

    fixed jump sense
    lowered velocity of jump

--- womp.js
+++ womp.js
@@ -49,27 +49,51 @@ window.addEventListener('load', function(){
             // speed on its own controls player movement with <1 moving us right and >1(negatives) moving us left
             //attaching key functions to speed will allow user to control player
             this.speed = 0;
+            this.vy = 0;
+            // need to keep player from flying off the display screen
+            this.gravity = 1;
+
         }
         draw(context){
             context.fillStyle = 'white'
             context.fillRect(this.x, this.y, this.width, this.height);
-            context.drawImage(this.image, this.x, this.y);
+            context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height,
+                 this.width, this.height, this.x, this.y, this.width, this.height);
             // draw image context would be more used if i uploaded the entire sprite sheet of the png files for player as well as enemies 
             // for now for the project deadlines sake its going to be kept super simple since i chose to crop one sprite from the sheet 
         }
         update(input){
             // input is taken from line 10 and passed here and then to player update in animate function
-            this.x += this.speed;
             if (input.keys.indexOf('ArrowRight') > -1){
                 this.speed = 5;
             } else if (input.keys.indexOf('ArrowLeft') > -1){
-                this.speed= -5;
+                this.speed = -5;
+                // jumping animation will be handled relatively the same way
+            } else if (input.keys.indexOf('ArrowUp') > -1){
+                // velocity y
+                this.vy -= 5;
+
             } else {
                 // brings player speed back to zero after initial key movement
                 this.speed = 0;
             }
+            this.x += this.speed;
+
             if (this.x < 0) this.x = 0;
             else if (this.x > this.playerWidth - this.width) this.x = this.playerWidth - this.width
+        //    comment to break horizontal and vertical 
+            this.y += this.vy;
+            if (!this.onFloor()) {
+                this.vy += this.gravity;
+            } else {
+                this.vy = 0;
+            }
+            if (this.y > this.playerHeight - this.height) this.y = this.playerHeight - this.height
+
+        }
+        // need to tell when player is or is on the ground
+        onFloor(){
+            return this.y >= this.playerHeight - this.height;
         }
 
     }
